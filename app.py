4683c7ebb7cad7c1728bc9dc63acf57e9522089d
manjunath1219 <117359394+manjunath1219@users.noreply.github.com>
2024-07-09 10:45:13 +0530

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 ###  Import the libraries  ###
 
-
 import pandas as pd
 import numpy as np
 import streamlit as st
 
 ### Step 1: Loading the Data
+
 st.file_uploader("Upload your input file", type=["csv"], key="uploaded_file")
 data = pd.read_table(st.session_state["uploaded_file"] , sep=",", header=0)
 st.dataframe(data)
@@ -13,7 +13,6 @@ st.dataframe(data)
 ### Step 2: Data Cleaning
 missing_values = data.isnull().sum()
 print(missing_values)
-
 
 # Drop rows with missing values or fill them with appropriate values
 
@@ -24,6 +23,7 @@ data['Date'] = pd.to_datetime(data['Date'])
 
 # Display cleaned data
 data.columns
+
 ### Step 3: Exploratory Data Analysis (EDA)
 
 # Summary statistics
@@ -55,33 +55,43 @@ print(avg_revenue_per_category)
 
 ######  Deploying the model  ######
 
+# Display the data
 st.title('Reading the Data')
 st.dataframe(data)
 
+# Display the Missing Values
 st.title('Missing Values')
 st.dataframe(missing_values)
 
+# Display the Summary statistics like mean, median etc
 st.title('Summary statistics')
 st.dataframe(summary_stats)
 
+# Display the Group by Category and calculate total sales
 st.title('Group by Category and calculate total sales')
 st.dataframe(category_sales)
 
+# Display the Total revenue
 st.title('Calculate total revenue')
 st.text(total_revenue)
 
+# Display the Monthly sales trend
 st.title('Monthly sales trend')
 st.dataframe(monthly_sales)
 
+# Display the Correlation between Price and Quantity Sold
 st.title('Correlation between Price and Quantity Sold')
 st.text( correlation)
 
+# Display the Average revenue per product category
 st.title('Average revenue per product category')
 st.dataframe(avg_revenue_per_category)
 
+# Display the Barplot of Sales by category
 st.title('Barplot of Sales by category')
 st.bar_chart(category_sales, x='Product Category', y='Units Sold')
 
+# Display the Lineplot of Monthly sales trend
 st.title('Lineplot of Monthly sales trend')
 st.line_chart(monthly_sales, x='Date', y='Units Sold' )
 
